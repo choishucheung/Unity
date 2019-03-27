3731299e89353d818d4aa7b5600ec168fc58bce0
choishucheung <choishucheung@qq.com>
2019-03-27 08:26:01 +0800

    状态模式上传

---
+++ RedAlertProject/RedAlertProject/Assets/DesignModeCode/01StateDesignMode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Context
+{
+    private IState _state;
+    public void SetState(IState state){
+        _state = state;
+    }
+}
+
+public interface IState
+{
+    void Handle(int arg);
+}
+
+/// <summary>
+/// 具体的子类
+/// </summary>
+public class ConcreteStateA:IState
+{
+    public void Handle(int arg)
+    {
+        Debug.Log(1);
+    }
+}
+
+public class ConcreteStateB : IState
+{
+    public void Handle(int arg)
+    {
+        Debug.Log(2);
+    }
+}
